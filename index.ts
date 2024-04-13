c1419c5d5c64d80de78c97587c5cc1c6f8fb05a2
Simon Emanuel Schmid <simon@schmid.io>
2024-04-13 16:48:23 +0100

    Basic GraphQL Yoga setup with Bun

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import { createSchema, createYoga } from "graphql-yoga";
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        greetings: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        greetings: () => "Hello Bun!",
+      },
+    },
+  }),
+});
+
+const server = Bun.serve(yoga as any);
+
+console.info(
+  `Server is running on http://${server.hostname}:${server.port}${yoga.graphqlEndpoint}`
+);
